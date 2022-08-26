ca95d21e610344680b06b2069fb1b6bb4a1d909d
Sebastian Ospina <juansebastian200103@gmail.com>
2022-08-26 19:28:25 -0400

    Create service implementation

---
+++ src/main/java/edu/eci/ieti/service/implementation/CustomerServiceHashMap.java
@@ -0,0 +1,43 @@
+package edu.eci.ieti.service.implementation;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import edu.eci.ieti.entity.Customer;
+import edu.eci.ieti.service.CustomerService;
+
+@Service
+public class CustomerServiceHashMap implements CustomerService {
+
+    @Override
+    public Customer create(Customer user) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Customer findById(String id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Customer> getAll() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void deleteById(String id) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Customer update(Customer user, String userId) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
